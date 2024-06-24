a0d0c646ecbd49326bd77b792383001f97448bc8
Bruno Holanda <brunoholanda128@gmail.com>
2024-06-24 20:16:02 -0300

    atualização do PropertyDropdown

--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,34 +1,52 @@
-import React, { createContext, useState, useEffect } from 'react';
-import { housesData } from '../data';
+import React, { createContext, useState, useEffect } from "react";
+import { housesData } from "../data";
 
 export const HouseContext = createContext();
 
 const HouseContextProvider = ({ children }) => {
-  //houseData é onde fica a lista de descrição dos imoveis.
-  
+  // houseData é onde fica a lista de descrição dos imóveis.
+
   const [houses, setHouses] = useState(housesData);
-  const [country, setCountry] = useState('Localização');
+  const [country, setCountry] = useState("Localização");
   const [countries, setCountries] = useState([]);
-  const [property, setProperty] = useState('Tipo de imovel');
+  const [property, setProperty] = useState("Tipo de imóvel");
   const [properties, setProperties] = useState([]);
-  const [price, setPrice] = useState('Valores de imoveis');
+  const [price, setPrice] = useState("Valores de imóveis");
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const allCountries = houses.map((house) => house.country);
-    console.log(allCountries);
+    const allCountries = houses.map((house) => {
+      return house.country;
+    });
 
     // Remover duplicatas
-    const uniqueCountries = ['Location (any)', ...new Set(allCountries)];
+    const uniqueCountries = ["Localização (any)", ...new Set(allCountries)];
 
     // Definir estado dos países
     setCountries(uniqueCountries);
-  }, []);
-  const handleClick=()=>{
- console.log('clicked');
-  }
+  }, [houses]);
+
+  useEffect(() => {
+    const allProperties = houses.map((house) => {
+      return house.type;
+    });
+
+    // Remover duplicatas
+    const uniqueProperties = [
+      "Tipo de imóvel (any)",
+      ...new Set(allProperties),
+    ];
+
+    // Definir estado dos tipos de propriedade
+    setProperties(uniqueProperties);
+  }, [houses]);
+
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
   return (
-    <HouseContext.Provider 
+    <HouseContext.Provider
       value={{
         country,
         setCountry,
